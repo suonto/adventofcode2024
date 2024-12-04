35d95ebd26a6f598ed1014699a09e864e4caf489
Markus Suonto <markus.suonto@gmail.com>
2024-12-04 14:38:25 +0000

    initial commit

---
+++ include/01/main.h
@@ -0,0 +1,6 @@
+#ifndef AOC_2024_01_H
+#define AOC_2024_01_H
+
+uint8_t solve();
+
+#endif
---
+++ src/01/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "01/main.h"
+
+uint8_t solve() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
